3cbc6133ef32f9d9c2265036a44ed067cfb52fbb
taher33 <taherhehe@gmail.com>
2021-08-18 18:57:32 +0100

    finnaly made it work the way I want

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const http = require("http");
+const { addUser, getUser } = require("./users");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
@@ -19,13 +20,26 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("hey 2222 there");
+  res.send("hey there");
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("room", ({ room, name }) => {
+    const { error, success } = addUser({ id: socket.id, name, room });
+    console.log("error", error);
+    console.log("success", success);
+    socket.join("room");
+  });
+  // io.of("/").adapter.on("join-room", (room, id) => {
+  //   console.log(`socket ${id} has joined room ${room}`);
+  // });
+  socket.on("message", ({ name, message }) => {
+    const user = getUser(name);
+
+    socket.to(user.id).emit("message", message);
+  });
 });
 
 server.listen(5000, () => {
-  console.log("listening on *:5000");
+  console.log("listening on port:5000");
 });
